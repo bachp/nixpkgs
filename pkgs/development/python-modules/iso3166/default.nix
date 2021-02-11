db6fb4c6ebc5c129f510701b76e8aba7d6b13ef4
Pascal Bach <pascal.bach@nextrem.ch>
2021-02-11 23:43:18 +0100

    python3Packages.iso3166: 0.8 -> 1.0.1
    Also clean up a bit:
    - fetch the specified version from Pypi
    - use pytestCheckHook

--- pkgs/development/python-modules/iso3166/default.nix
+++ pkgs/development/python-modules/iso3166/default.nix
@@ -1,22 +1,15 @@
-{ lib, fetchFromGitHub, buildPythonPackage, pytest }:
+{ lib, fetchPypi, buildPythonPackage, pytestCheckHook }:
 
-buildPythonPackage {
+buildPythonPackage rec {
   pname = "iso3166";
-  version = "0.8";
+  version = "1.0.1";
 
-  src = fetchFromGitHub {
-    owner = "deactivated";
-    repo = "python-iso3166";
-    # repo has no version tags
-    rev = "f04e499447bbff10af701cf3dd81f6bcdf02f7d7";
-    sha256 = "0zs9za9dr2nl5srxir08yibmp6nffcapmzala0fgh8ny7y6rafrx";
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "1ig8v9lsf6z7f8pqbb12z2w31nqcgw2sdiwf86f2rfqgyny8vrdi";
   };
 
-  checkInputs = [ pytest ];
-
-  checkPhase = ''
-    py.test
-  '';
+  checkInputs = [ pytestCheckHook ];
 
   meta = with lib; {
     homepage = "https://github.com/deactivated/python-iso3166";
